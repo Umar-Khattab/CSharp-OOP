a626c95f667482ab6c9362f003609a43135b0f00
omar khattab <omar.khattab7@icloud.com>
2024-03-30 09:17:13 +0200

    Default constructor implementation

--- CSharp-OOP/Program.cs
+++ CSharp-OOP/Program.cs
@@ -15,7 +15,9 @@ namespace CSharp_OOP
          *costructor(parameters)
          *
          * }*/
-        public class Students
+
+        // Default constructor
+        public class Students (int id , int number , string name , string classnum)
         {
             private int id;
             private int number;
@@ -52,41 +54,6 @@ namespace CSharp_OOP
                 get { return this.classnum; }
             }*/
                 
-            //create a consturctor which take a parmeters and make overloading
-            public Students(int i ,int n ,string N , string c)
-            {
-                id = i;
-                number = n;
-                name = N;
-                classnum = c;
-
-            }  //frist
-            public Students(int i, int n, string N)
-            {
-                id = i;
-                number = n;
-                name = N;
-               
-
-            } //dif sign
-            public Students(int i, int n)
-            {
-                id = i;
-                number = n;
-              
-
-            }
-            public Students(int i)
-            {
-                id = i;
-             
-
-            }
-            public Students()
-            {
-           
-
-            }
 
             public void Display()
             {
